edb776fb3cdcb91f7381c47aab05f22ce8e5c910
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-09-05 16:10:56 -0400

    add a mapping profile. now the api route works

---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using vega.Models;
+using vega.Controllers.Resources;
+
+namespace vega.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Make, MakeResource>();
+            CreateMap<Model, ModelResource>();
+        }
+    }
+}
