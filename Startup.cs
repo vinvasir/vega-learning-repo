69b183ed6cebae726b89fa975633d534de7a5ee9
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-09-05 15:05:34 -0400

    use the db context configuration in the startup.cs file

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer("..."));
+            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:Default"]));
             services.AddMvc();
         }
 
