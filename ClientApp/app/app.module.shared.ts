81df4f595d2245d4ccde4695ceb220668cefbf1d
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-09-06 11:47:54 -0400

    set up angular-cli, with ClientApp/app as the source directory. use ng g c [component-name] -m app.module.shared.ts to generate components

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { VehicleFormComponent } from './app/components/vehicle-form/vehicle-form.component';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        VehicleFormComponent
     ],
     imports: [
         CommonModule,
