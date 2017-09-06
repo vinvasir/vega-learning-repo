9eec7d945232f6c3a91ac50bcbe3acb5c1a97066
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-09-06 10:47:42 -0400

    add data constraints on the feature model

--- Models/Feature.cs
+++ Models/Feature.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace vega.Models
@@ -6,6 +7,9 @@ namespace vega.Models
     public class Feature
     {
         public int Id { get; set; }
+        
+        [Required]
+        [StringLength(255)]        
         public string Name { get; set; }
     }
 }
