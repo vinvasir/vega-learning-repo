de335efb6e44cba4ce66e5ad7e13a139a0bfe3f4
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-09-06 10:58:13 -0400

    create a feature resource, and add the asyncronous database call and DataMapper serialization

--- Controllers/FeaturesController.cs
+++ Controllers/FeaturesController.cs
@@ -1,4 +1,10 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vega.Controllers.Resources;
+using vega.Models;
 using vega.Persistence;
 
 namespace vega.Controllers
@@ -15,7 +21,9 @@ namespace vega.Controllers
         [HttpGet("/api/features")]
         public async Task<IEnumerable<FeatureResource>> GetFeatures()
         {
+            var features = await context.Features.ToListAsync();
 
+            return Mapper.Map<List<Feature>, List<FeatureResource>>(features);
         }
     }
 }
