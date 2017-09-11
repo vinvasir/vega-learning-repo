9fee7f85bbe4284302215568c2829961c8ee18b3
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-09-11 14:17:15 -0400

    set up a mapping from a JSON vehicle resource to a vehicle domain object

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using vega.Models;
 using vega.Controllers.Resources;
+using System.Linq;
 
 namespace vega.Mapping
 {
@@ -8,9 +9,19 @@ namespace vega.Mapping
     {
         public MappingProfile()
         {
+            // Domain model to serialized API resource
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Feature, FeatureResource>();
+
+            // Serialized API resource to domain model
+            CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+                .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+                .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => 
+                    new VehicleFeature { FeatureId = id }
+                )));
         }
     }
 }
