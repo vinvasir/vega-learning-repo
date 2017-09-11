e5d735d697342bbc6106f6f79945860e26529ff0
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-09-11 14:12:16 -0400

    add the contactresource

---
+++ Controllers/Resources/ContactResource.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace vega.Controllers.Resources
+{
+    public partial class VehicleResource
+    {
+        public class ContactResource
+        {
+            [Required]
+            [StringLength(255)]
+            public string Name { get; set; }
+
+            [StringLength(255)]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(255)]
+            public string Phone { get; set; }
+        }
+    }
+}
