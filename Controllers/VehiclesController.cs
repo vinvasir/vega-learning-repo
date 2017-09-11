1b74a9935cbbc047365c02c15e607476bd834040
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-09-11 14:51:14 -0400

    inject the db context into the controller, and set up a vehicle db set in the context class. then call that vehicle db set property to add the newed-up vehicle, and save changes

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,14 +1,31 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using vega.Controllers.Resources;
 using vega.Models;
+using vega.Persistence;
 
 namespace vega.Controllers
 {
     [Route("/api/vehicles")]
     public class VehiclesController : Controller
     {
-        [HttpPost]
-        public IActionResult CreateVehicle(Vehicle vehicle)
+        private readonly IMapper mapper;
+        private readonly VegaDbContext context;
+
+        public VehiclesController(IMapper mapper, VegaDbContext context)
         {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+
+            context.Vehicles.Add(vehicle);
+            context.SaveChanges();
+
             return Ok(vehicle);
         }
     }
